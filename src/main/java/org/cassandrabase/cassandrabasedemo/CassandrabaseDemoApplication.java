fbb7e42b12206ac9c05cc1315af317797d435bdf
mafei <mafei.dev@gmail.com>
2024-10-15 15:38:10 +0530

    added Cassandrabase

--- src/main/java/org/cassandrabase/cassandrabasedemo/CassandrabaseDemoApplication.java
+++ src/main/java/org/cassandrabase/cassandrabasedemo/CassandrabaseDemoApplication.java
@@ -1,13 +1,27 @@
 package org.cassandrabase.cassandrabasedemo;
 
+import com.datastax.oss.driver.api.core.CqlSession;
+import lombok.RequiredArgsConstructor;
+import org.cassandrabase.lite.Cassandrabase;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
-public class CassandrabaseDemoApplication {
+@RequiredArgsConstructor
+public class CassandrabaseDemoApplication implements CommandLineRunner {
+    private final CqlSession cqlSession;
 
     public static void main(String[] args) {
         SpringApplication.run(CassandrabaseDemoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        try (Cassandrabase cassandrabase = new Cassandrabase(cqlSession, UUID.randomUUID().toString(), this)) {
+            cassandrabase.validate();
+        }
+    }
 }
