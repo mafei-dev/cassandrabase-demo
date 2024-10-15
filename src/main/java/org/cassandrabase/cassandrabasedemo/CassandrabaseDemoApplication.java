df65e6f5e78619220fb1724b1fbc3d33b52df6f0
mafei <mafei.dev@gmail.com>
2024-10-15 14:43:37 +0530

    Initial commit

---
+++ src/main/java/org/cassandrabase/cassandrabasedemo/CassandrabaseDemoApplication.java
@@ -0,0 +1,13 @@
+package org.cassandrabase.cassandrabasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandrabaseDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CassandrabaseDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cassandrabase/cassandrabasedemo/CassandrabaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cassandrabase.cassandrabasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandrabaseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
